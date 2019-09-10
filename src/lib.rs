666ddf52b7c69509ad2fd72bd1853e0bc31746d6
Ricky <Ricky@Hosfelt.io>
2019-09-10 12:42:05 -0400

    Change API calls to make a bit more sense

--- src/lib.rs
+++ src/lib.rs
@@ -15,107 +15,107 @@ pub enum Error {
 }
 
 pub trait Duration {
-    fn to_us(self) -> Result<time::Duration, Error>;
-    fn to_ms(self) -> Result<time::Duration, Error>;
-    fn to_ns(self) -> Result<time::Duration, Error>;
-    fn to_sec(self) -> Result<time::Duration, Error>;
+    fn micros(self) -> Result<time::Duration, Error>;
+    fn millis(self) -> Result<time::Duration, Error>;
+    fn nanos(self) -> Result<time::Duration, Error>;
+    fn sec(self) -> Result<time::Duration, Error>;
 }
 
 impl Duration for u8 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_micros(self.into()))
     } 
 
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_millis(self.into()))
     }
 
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_nanos(self.into()))
     }
 
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_secs(self.into()))
     }
 }
 
 impl Duration for u16 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_micros(self.into()))
     } 
 
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_millis(self.into()))
     }
 
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_nanos(self.into()))
     }
 
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_secs(self.into()))
     }
 }
 
 impl Duration for u32 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_micros(self.into()))
     } 
 
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_millis(self.into()))
     }
 
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_nanos(self.into()))
     }
 
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_secs(self.into()))
     }
 }
 
 impl Duration for u64 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_micros(self))
     } 
 
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_millis(self))
     }
 
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_nanos(self))
     }
 
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         Ok(time::Duration::from_secs(self))
     }
 }
 
 impl Duration for u128 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         if self > u64::max_value() as u128 {
             Err(Error::OverFlow{number: self})
         } else {
             Ok(time::Duration::from_micros(self as u64))
         }
     } 
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         if self > u64::max_value() as u128 {
             Err(Error::OverFlow{number: self})
         } else {
             Ok(time::Duration::from_millis(self as u64))
         }
     } 
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         if self > u64::max_value() as u128 {
             Err(Error::OverFlow{number: self})
         } else {
             Ok(time::Duration::from_nanos(self as u64))
         }
     } 
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         if self > u64::max_value() as u128 {
             Err(Error::OverFlow{number: self})
         } else {
@@ -125,28 +125,28 @@ impl Duration for u128 {
 }
 
 impl Duration for i8 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_micros(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_millis(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_nanos(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_secs(self as u64))
         } else {
@@ -157,28 +157,28 @@ impl Duration for i8 {
 
 
 impl Duration for i16 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_micros(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_millis(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_nanos(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_secs(self as u64))
         } else {
@@ -189,28 +189,28 @@ impl Duration for i16 {
 
 
 impl Duration for i32 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_micros(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_millis(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_nanos(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_secs(self as u64))
         } else {
@@ -220,28 +220,28 @@ impl Duration for i32 {
 }
 
 impl Duration for i64 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_micros(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_millis(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_nanos(self as u64))
         } else {
              Err(Error::Convert{number: self as i128})
         }
     }
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         if self > 0 {
             Ok(time::Duration::from_secs(self as u64))
         } else {
@@ -251,7 +251,7 @@ impl Duration for i64 {
 }
 
 impl Duration for i128 {
-    fn to_us(self) -> Result<time::Duration, Error> {
+    fn micros(self) -> Result<time::Duration, Error> {
         if self > 0 {
             if self > u64::max_value() as i128 {
                 Err(Error::OverFlow{number: self as u128})
@@ -262,7 +262,7 @@ impl Duration for i128 {
             Err(Error::Convert{number:self})
         }
     }
-    fn to_ms(self) -> Result<time::Duration, Error> {
+    fn millis(self) -> Result<time::Duration, Error> {
         if self > 0 {
             if self > u64::max_value() as i128 {
                 Err(Error::OverFlow{number: self as u128})
@@ -273,7 +273,7 @@ impl Duration for i128 {
             Err(Error::Convert{number:self})
         }
     }
-    fn to_ns(self) -> Result<time::Duration, Error> {
+    fn nanos(self) -> Result<time::Duration, Error> {
         if self > 0 {
             if self > u64::max_value() as i128 {
                 Err(Error::OverFlow{number: self as u128})
@@ -284,7 +284,7 @@ impl Duration for i128 {
             Err(Error::Convert{number:self})
         }
     }
-    fn to_sec(self) -> Result<time::Duration, Error> {
+    fn sec(self) -> Result<time::Duration, Error> {
         if self > 0 {
             if self > u64::max_value() as i128 {
                 Err(Error::OverFlow{number: self as u128})
--- tests/test.rs
+++ tests/test.rs
@@ -5,90 +5,90 @@ use std::{thread, time};
 fn test_milliseconds() -> Result<(), tarde::Error>{
     let x = 10;
     let now = time::Instant::now();
-    thread::sleep(x.to_ms()?); 
+    thread::sleep(x.millis()?); 
 
-    Ok(assert!(now.elapsed() >= x.to_ms()?))
+    Ok(assert!(now.elapsed() >= x.millis()?))
 }
 
 #[test]
 #[should_panic]
 fn test_milliseconds_overflow() {
     let x: u128 = 184467440737095516199999;
-    x.to_ms().unwrap(); 
+    x.millis().unwrap(); 
 } 
 
 #[test]
 #[should_panic]
 fn test_negative_milliseconds() {
     let x: i32 = -42;
-    x.to_ms().unwrap(); 
+    x.millis().unwrap(); 
 }
 
 #[test]
 fn test_microseconds() -> Result<(), tarde::Error> {
     let x = 10; 
     let now = time::Instant::now();
-    thread::sleep(x.to_us()?);
+    thread::sleep(x.micros()?);
     
-    Ok(assert!(now.elapsed() >= x.to_us()?))
+    Ok(assert!(now.elapsed() >= x.micros()?))
 }
 
 #[test]
 #[should_panic]
 fn test_microseconds_overflow() {
     let x: u128 = 18446744073709551614599999;
-    x.to_us().unwrap();
+    x.micros().unwrap();
 }
 
 #[test]
 #[should_panic]
 fn test_microseconds_negative() {
     let x: i32 = -42;
-    x.to_us().unwrap();
+    x.micros().unwrap();
 }
 
 #[test]
 fn test_nanoseconds() -> Result<(), tarde::Error> {
     let x = 10; 
     let now = time::Instant::now();
-    thread::sleep(x.to_ns()?);
+    thread::sleep(x.nanos()?);
     
-    Ok(assert!(now.elapsed() >= x.to_ns()?))
+    Ok(assert!(now.elapsed() >= x.nanos()?))
 }
 
 #[test]
 #[should_panic]
 fn test_nanoseconds_overflow() {
     let x: u128 = 18446744073709551614599999;
-    x.to_ns().unwrap();
+    x.nanos().unwrap();
 }
 
 #[test]
 #[should_panic]
 fn test_nanoseconds_negative() {
     let x: i32 = -42;
-    x.to_ns().unwrap();
+    x.nanos().unwrap();
 }
 
 #[test]
 fn test_seconds() -> Result<(), tarde::Error> {
     let x = 1; 
     let now = time::Instant::now();
-    thread::sleep(x.to_sec()?);
+    thread::sleep(x.sec()?);
     
-    Ok(assert!(now.elapsed() >= x.to_sec()?))
+    Ok(assert!(now.elapsed() >= x.sec()?))
 }
 
 #[test]
 #[should_panic]
 fn test_seconds_overflow() {
     let x: u128 = 18446744073709551614599999;
-    x.to_sec().unwrap();
+    x.sec().unwrap();
 }
 
 #[test]
 #[should_panic]
 fn test_seconds_negative() {
     let x: i32 = -42;
-    x.to_sec().unwrap();
+    x.sec().unwrap();
 }
